1463f908a0a0998b00719cf2ed58c6a52aa87d00
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-08-15 19:54:31 -0400

    Adds product mantainer page

---
+++ src/app/(shop)/admin/products/page.tsx
@@ -0,0 +1,113 @@
+export const dynamic = 'force-dynamic';
+
+import { getPaginatedProductsWithImages } from '@/actions';
+import { Pagination, Title } from '@/components';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { IoCardOutline } from 'react-icons/io5';
+
+interface Props {
+  searchParams: { page: string };
+}
+
+export default async function AdminProductsPage({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+
+  const { products, totalPages } = await getPaginatedProductsWithImages({
+    page,
+  });
+
+  return (
+    <>
+      <Title title='Mantenedor productos' />
+      <div className='flex justify-end mb-5'>
+        <Link href={'/admin/product/new'} className='btn-primary'>
+          Nuevo producto
+        </Link>
+      </div>
+
+      <div className='mb-10'>
+        <table className='min-w-full'>
+          <thead className='bg-gray-200 border-b'>
+            <tr>
+              <th
+                scope='col'
+                className='text-sm font-medium text-gray-900 px-6 py-4 text-left'
+              >
+                Imagen
+              </th>
+              <th
+                scope='col'
+                className='text-sm font-medium text-gray-900 px-6 py-4 text-left'
+              >
+                Titulo
+              </th>
+              <th
+                scope='col'
+                className='text-sm font-medium text-gray-900 px-6 py-4 text-left'
+              >
+                Precio
+              </th>
+              <th
+                scope='col'
+                className='text-sm font-medium text-gray-900 px-6 py-4 text-left'
+              >
+                Genero
+              </th>
+              <th
+                scope='col'
+                className='text-sm font-medium text-gray-900 px-6 py-4 text-left'
+              >
+                Inventario
+              </th>
+              <th
+                scope='col'
+                className='text-sm font-medium text-gray-900 px-6 py-4 text-left'
+              >
+                Tallas
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {products?.map((product) => (
+              <tr
+                key={product.id}
+                className='bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100'
+              >
+                <td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>
+                  <Link href={`/product/${product.slug}`}>
+                    <Image
+                      src={`/products/${product.ProductImage[0].url}`}
+                      width={80}
+                      height={80}
+                      alt={product.title}
+                      className='rounded object-cover'
+                    />
+                  </Link>
+                </td>
+                <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
+                  {product.title}
+                </td>
+                <td className='text-sm  text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
+                  {product.price}
+                </td>
+                <td className='text-sm text-gray-900 font-light px-6 '>
+                  {product.gender}
+                </td>
+                <td className='text-sm text-gray-900 font-light px-6 '>
+                  {product.inStock}
+                </td>
+                <td className='text-sm text-gray-900 font-light px-6 '>
+                  {product.sizes.join(', ')}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <Pagination totalPages={totalPages ?? 0} />
+      </div>
+    </>
+  );
+}
