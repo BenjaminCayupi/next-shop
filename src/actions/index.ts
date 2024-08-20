036db43f92f8c7e24e287ae6b15a76a08a1a22e9
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-08-19 22:14:15 -0400

    Adds delete product images action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,6 +2,7 @@ export { getProductBySlug } from './product/get-product-by-slug';
 export { getStockBySlug } from './product/get-stock-by-slug';
 export { getPaginatedProductsWithImages } from './product/product-pagination';
 export { createUpdateProduct } from './product/create-update-product';
+export { deleteProductImage } from './product/delete-product-image';
 
 export { authenticate } from './auth/login';
 export { logout } from './auth/logout';
