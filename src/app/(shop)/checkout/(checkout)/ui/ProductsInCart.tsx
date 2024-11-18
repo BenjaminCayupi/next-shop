dae35b8782eda29872bbc45bbabac0049403ec86
benjaminCayupi <benjamin.cayupi@gmail.com>
2024-11-18 13:36:38 -0300

    Fix checkout image

--- src/app/(shop)/checkout/(checkout)/ui/ProductsInCart.tsx
+++ src/app/(shop)/checkout/(checkout)/ui/ProductsInCart.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { useCartStore } from '@/store';
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useCartStore } from "@/store";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export const ProductsInCart = () => {
   const productsInCart = useCartStore((state) => state.cart);
@@ -19,26 +19,26 @@ export const ProductsInCart = () => {
   return (
     <>
       {productsInCart.map((product) => (
-        <div key={`${product.slug}-${product.size}`} className='flex mb-5'>
+        <div key={`${product.slug}-${product.size}`} className="flex mb-5">
           <Image
-            src={`/products/${product.image}`}
+            src={product.image}
             width={100}
             height={100}
             style={{
-              width: '100px',
-              height: '100px',
+              width: "100px",
+              height: "100px",
             }}
             alt={product.title}
-            className='mr-5 rounded'
+            className="mr-5 rounded"
           />
           <div>
             <span>
-              <p className='font-normal'>
+              <p className="font-normal">
                 {product.title} - Talla : {product.size}
               </p>
             </span>
-            <p className='font-normal'>Cantidad: {product.quantity}</p>
-            <p className='font-bold'>${product.price * product.quantity}</p>
+            <p className="font-normal">Cantidad: {product.quantity}</p>
+            <p className="font-bold">${product.price * product.quantity}</p>
           </div>
         </div>
       ))}
